e207ce4481a7cd961836b7f4b6c0f740468b1d23
Aryan Shourya <aryanshourya98@gmail.com>
2025-01-10 23:25:06 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import os
+from langchain.chains.question_answering.map_reduce_prompt import system_template
+from langchain.chains.summarize.refine_prompts import prompt_template
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("MISTRAL_API_KEY")
+llm = ChatMistralAI(api_key=api_key)
+
+# summarization part
+def summarize_text(text_input):
+
+    system_template = "Generate a concise summary of the following text."
+
+    prompt_template = ChatPromptTemplate.from_messages(
+        [("system",system_template), ("user" ,"{text}")]
+    )
+
+    prompt = prompt_template.invoke({"text": text_input})
+    response = llm.invoke(prompt).content
+    return response
+
+
+
+# Tittle of the page
+def main():
+    st.title("Summary Generator")
+    st.subheader("Input \n")
+    text_input = st.text_area("Enter paragraph",max_chars=2000,height=100,value="",key="textbox")
+
+    sum = st.button("Summary")
+    result = summarize_text(text_input)
+    if sum:
+         st.text_area("Here is your summary:",value=result,height=200)
+
+if __name__ == "__main__":
+    main()
